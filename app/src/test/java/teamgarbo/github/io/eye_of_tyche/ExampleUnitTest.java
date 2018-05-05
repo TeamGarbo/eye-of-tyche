c41bd71067d4ef3a1d039f19ee82914cf27ec746
ShantnuS <shantnu@outlook.com>
2018-05-05 19:06:45 +0100

    initial android files

---
+++ app/src/test/java/teamgarbo/github/io/eye_of_tyche/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package teamgarbo.github.io.eye_of_tyche;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
