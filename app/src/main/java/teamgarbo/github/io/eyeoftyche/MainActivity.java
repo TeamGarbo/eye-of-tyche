27c43367102ceddeeffc80870d3ea9373828cd2d
ShantnuS <shantnu@outlook.com>
2018-05-05 20:08:51 +0100

    permissions and vibrate

--- app/src/main/java/teamgarbo/github/io/eyeoftyche/MainActivity.java
+++ app/src/main/java/teamgarbo/github/io/eyeoftyche/MainActivity.java
@@ -1,6 +1,8 @@
 package teamgarbo.github.io.eyeoftyche;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -19,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void init(){
         Controller.getInstance();
-
+        vibrate(1000);
         //Initialise global variables, get permissions, etc.
 
         initMainMenu();
@@ -28,5 +30,11 @@ public class MainActivity extends AppCompatActivity {
     public void initMainMenu(){
         Intent myIntent = new Intent(MainActivity.this, MainMenuActivity.class);
         startActivity(myIntent);
+    }
+
+    public void vibrate(int millis){
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        v.vibrate(millis);
     }
 }
