dfaf7b1af8f7a889a740ec2abc7a5728ad3949e8
ShantnuS <shantnu@outlook.com>
2018-05-06 11:06:07 +0100

    names

--- app/src/main/java/teamgarbo/github/io/eyeoftyche/Engine/Globals.java
+++ app/src/main/java/teamgarbo/github/io/eyeoftyche/Engine/Globals.java
@@ -17,6 +17,12 @@ public class Globals {
     static public final int ITEM_ARMOUR = 0;
     static public final int ITEM_WEAPON = 1;
     static public final int ITEM_CONSUMABLE = 2;
+    static public String[] rooms = {"Cave","Field","Kitchen","Corridor","Hall","Forest","Woods","Pathway","Castle","Village","Cottage","Beach","Seaside","Lakeside","Town","City","Mine","Mountain","Desert","Tundra","Swamp","Corrupted Lands","Infested Lands","Diseased Lands","Plagued Lands","Grasslands","Flatlands","Hills","Farm"};
+    static public String[] adjectives = {"Creepy","Old","Dangerous","Evil","Awesome","Great","Broken","Haunted","Deadly","Spooky","Lucky","Mystical","Amazing","Wonderous","Brilliant","Poisonous ","Loud"};
+    static public String[] armour = {"breastplate","legplate","helm","helmet","robe","torso","boots","gloves","tunic","scarf","hat","greaves","leather belt","hide chest","hide legs","hide boots","coif","plated gloves","full helm","spiked helm"};
+    static public String[] weapons= {"mace","dagger","sword","long sword","bow","long bow","curved bow","recurve bow","bone bow","short bow","short sword","scimitar","axe","hachet","flail","morning star","shurikan","whip","rocks","potion"};
+    static public String[] con = {"life potion","mana potion","hybrid potion","apple","banana","orange","chocolate","chicken","beef","lamb","pork","steak","potatoes","carrots"};
+
 
 
 }
